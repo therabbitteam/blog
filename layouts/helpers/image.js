6cce209bd0dc80180451dc51eaea09451de56875
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-19 11:38:18 +0700

    Can't make resize to work

--- layouts/helpers/image.js
+++ layouts/helpers/image.js
@@ -1,15 +1,15 @@
-module.exports = function(object, width, height) {
+module.exports = function(object, width) {
   if (!object) {
     return;
   }
   // TODO: Auto download and manipulate image, save it to local file
   var image = object.json.main;
 
-  height = image.height || height;
-  width = width || image.width;
+  width = parseInt(width) || null;
+
   var url = image.url;
   var alt = image.alt;
 
-  return '<img src="'+ url +'" data-type="prismic" data-height="' + height + '" data-width="'
+  return '<img src="'+ url +'" data-type="prismic" data-width="'
               + width + '" alt="' + alt + '" />';
 };
