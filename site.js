10a34f2759a7a53be45a9b80d6c8d43d0a8f0130
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-20 22:56:23 +0700

    Use theme's scss files

--- site.js
+++ site.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       //"metalsmith-excerpts": {},
       "metalsmith-metadata": {
-        "sitedata": "config.yml"
+        "config": "config.yml"
       },
       "metalsmith-register-helpers": {
         "directory": "./layouts/helpers"
