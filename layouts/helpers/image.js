065220451e7d8a98b2ad1a437319904145873e21
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-21 22:40:59 +0700

    [UX] Don't scale up image

--- layouts/helpers/image.js
+++ layouts/helpers/image.js
@@ -6,7 +6,8 @@ module.exports = function(context, options) {
   var view = (_.has(options.hash, 'view')) ? options.hash.view : false;
   var image = context.json.main;
 
-  if (view) {
+  // Don't use upscale image from Prismic
+  if (view && image.width > getViewSize(context, view).width) {
     image = context.json.views[view];
   }
 
@@ -20,3 +21,28 @@ module.exports = function(context, options) {
 
   return new Handlebars.SafeString(result);
 };
+
+// Get Desire view size from image object
+function getViewSize(context, view) {
+  if (!view || view == 'main') {
+    var image = context.json.main;
+
+    return returnImageSize(image);
+  }
+
+   var views = context.json.views;
+
+  if (_.has(views, view)) {
+    return returnImageSize(views[view]);
+  }
+
+  throw new Error('Cannot find ' + view + ' view');
+}
+
+// Return height and width
+function returnImageSize(image) {
+  return {
+    height: image.height,
+    width: image.width
+  }
+}
