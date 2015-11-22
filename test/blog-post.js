813f5f4ae0f5b18013095be5f05ef1aa3839af3c
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-22 09:08:19 +0700

    Don't import font in css

---
+++ test/blog-post.js
@@ -0,0 +1,30 @@
+var connect = require('gulp-connect');
+var site = require('../site');
+var Browser = require('./browser').Browser;
+var PORT = require('./browser').PORT;
+
+describe('blog post', function() {
+  const browser = new Browser();
+
+  before(function() {
+    connect.server({
+      root: site.metalsmith.config.destRoot,
+      port: PORT
+    });
+  });
+
+  before(function() {
+    return browser.visit('/');
+  });
+
+  describe('connect successfully', function() {
+
+    it('should be successful', function() {
+      browser.assert.success();
+    });
+  });
+  after(function(done) {
+    connect.serverClose();
+    done();
+  });
+});
