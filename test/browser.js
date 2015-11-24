3f5c54a139ed3a0a7a75769209a579f33c52a1b0
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-24 21:17:01 +0700

    [Chore] Add more tests

--- test/browser.js
+++ test/browser.js
@@ -1,11 +1,13 @@
 var _ = require('lodash')
 
 const Browser = require('zombie')
+const domain = 'rabbitlearn.com'
 
 process.env.NODE_ENV = 'test'
 const PORT = _.random(5000, 6000)
 
-Browser.localhost('beta.rabbitlearn.com', PORT)
+Browser.localhost(domain, PORT)
 
 module.exports.Browser = Browser
 module.exports.PORT = PORT
+module.exports.domain = domain
